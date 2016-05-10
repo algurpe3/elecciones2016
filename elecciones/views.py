e232f4972f06aa2f8b0f3e50ecd1b66c3e755a5c
Al Gu <algurpe3@gmail.com>
2016-05-10 22:53:36 +0200

    Part 4.Template Inheritance, Website files and Testing with coverage

--- elecciones/views.py
+++ elecciones/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, "elecciones/index.html", {})
 
+def home_files(request, filename):
+    return render(request, filename, {}, content_type="text/plain")
+
+
+
