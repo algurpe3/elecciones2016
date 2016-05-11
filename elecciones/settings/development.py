7c25430452da8bb17f70a48f2358a4d1394e7c4f
Al Gu <algurpe3@gmail.com>
2016-05-11 21:06:59 +0200

    Broken state, auth.user permission error

--- elecciones/settings/development.py
+++ elecciones/settings/development.py
@@ -3,3 +3,14 @@ from .base import *
 
 # Add this removed variable from `base.py` parent settings template
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
